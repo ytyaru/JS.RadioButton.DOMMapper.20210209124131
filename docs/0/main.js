3e61decba70f3b417198894886e8994271649058
ytyaru <ytyaru@outlook.jp>
2021-02-13 14:26:41 +0900

    create

--- docs/0/main.js
+++ docs/0/main.js
@@ -1,24 +1,7 @@
-window.addEventListener('load', (event) => {
-    console.log(RadioButtonTag);
+window.addEventListener('load', async(event) => {
     RadioButtonTag.parse();
 
-    const mapper = new Mapper('./tsv/map.tsv');
-    await mapper.load();
-    console.log(mapper.Keys);
-    mapper.map('和');
-//    mapper.map('英');
-    createSelect();
-    function createSelect() {
-        const select = document.createElement('select');
-        for (let key of mapper.Keys) {
-            const option = document.createElement('option');
-            option.setAttribute('value', key);
-            option.textContent = key;
-            select.appendChild(option);
-        }
-        document.body.insertBefore(select, document.body.children[0]);
-        select.addEventListener('change', (event)=>{
-            mapper.map(event.target.value);
-        });
-    }
+    const select = new DomMapperSelect();
+    document.body.insertBefore(await select.create('./tsv/map.tsv'), document.body.children[0]);
+    console.log(select.Mapper, select.Element);
 });
